6152ecf90f190df48b5202c55f70326b029e763e
Mukul Kolpe <mukulkolpe45@gmail.com>
2023-06-03 16:30:59 +0530

    Added pages

---
+++ frontend/pages/apply_volunteer/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const applyVolunteer = () => {
+  return <div>applyVolunteer</div>;
+};
+
+export default applyVolunteer;
---
+++ frontend/pages/organize_event/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const organizeEvent = () => {
+  return <div>organizeEvent</div>;
+};
+
+export default organizeEvent;
